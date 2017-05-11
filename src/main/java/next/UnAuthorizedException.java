15bc3ec09e1058e8cbb9c053b1c365d2cbb62444
JaeYeopHan <ljyhanll@gmail.com>
2017-05-11 16:29:53 +0900

    Add UnAuthorizaedException class

---
+++ src/main/java/next/UnAuthorizedException.java
@@ -0,0 +1,10 @@
+package next;
+
+/**
+ * Created by Jbee on 2017. 5. 11..
+ */
+public class UnAuthorizedException extends Exception {
+    public UnAuthorizedException(String msg) {
+        super(msg);
+    }
+}
