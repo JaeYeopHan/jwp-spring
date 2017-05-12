9336bababc93b027554ffafadde770893746a644
JaeYeopHan <ljyhanll@gmail.com>
2017-05-12 10:57:08 +0900

    Add comment

--- src/main/java/next/config/WebMvcConfig.java
+++ src/main/java/next/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package next.config;
 
 import next.utils.LoginUserHandlerMethodArgumentResolver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -52,6 +51,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(new LoginUserHandlerMethodArgumentResolver());
+        // loginUserHandlerMethodArgumentResolver()은 이 클래스에 정의된 메소드가 호출되는 것이 아니라
+        // 이 클래스를 감싸고 있는 Proxy 클래스에 존재하는 loginUserHandlerMethodArgumentResolver()가 먼저 호출된다
+        // Proxy에서는 실행되는 메소드의 return type에 해당하는 Bean이 존재하는지를 확인하고
+        // 존재한다면 그 Bean을 return하고 존재하지 않으면 기존 클래스의 메소드를 그대로 호출한다.
+        argumentResolvers.add(loginUserHandlerMethodArgumentResolver());
     }
 }
