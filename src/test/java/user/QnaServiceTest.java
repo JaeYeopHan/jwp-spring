3c0862bfc716233b4095f5da9ac313595c7153e0
JaeYeopHan <ljyhanll@gmail.com>
2017-05-17 11:31:45 +0900

    Set up QnaServiceTest class

--- src/test/java/user/QnaServiceTest.java
+++ src/test/java/user/QnaServiceTest.java
@@ -1,9 +1,27 @@
 package user;
 
+import next.dao.AnswerDao;
+import next.dao.QuestionDao;
+import next.service.QnaService;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class QnaServiceTest {
+    @Mock
+    private QuestionDao questionDao;
+    @Mock
+    private AnswerDao answerDao;
+
+    private QnaService qnaService;
+
+    @Before
+    public void setUp() {
+        qnaService = new QnaService(questionDao, answerDao);
+    }
+
+    
 
 }
