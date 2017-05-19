7f46c68b87e4fd768081d232b710aab9bebbebd7
JaeYeopHan <ljyhanll@gmail.com>
2017-05-19 14:01:14 +0900

    Change anonymousr class -> lamda expression

--- src/main/java/next/dao/QuestionDao.java
+++ src/main/java/next/dao/QuestionDao.java
@@ -20,57 +20,43 @@ public class QuestionDao {
 
     public Question insert(Question question) {
         String sql = "INSERT INTO QUESTIONS (writer, title, contents, createdDate) VALUES (?, ?, ?, ?)";
-        PreparedStatementCreator psc = new PreparedStatementCreator() {
-
-			@Override
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				PreparedStatement pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, question.getWriter());
-				pstmt.setString(2, question.getTitle());
-				pstmt.setString(3, question.getContents());
-				pstmt.setTimestamp(4, new Timestamp(question.getTimeFromCreateDate()));
-				return pstmt;
-			}
-		};
-        
 		KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(psc, keyHolder);
+        jdbcTemplate.update((Connection con) -> {
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, question.getWriter());
+			pstmt.setString(2, question.getTitle());
+			pstmt.setString(3, question.getContents());
+			pstmt.setTimestamp(4, new Timestamp(question.getTimeFromCreateDate()));
+			return pstmt;
+		}, keyHolder);
+
         return findById((Long) keyHolder.getKey());
     }
 	
 	public List<Question> findAll() {
 		String sql = "SELECT questionId, writer, title, createdDate, countOfAnswer FROM QUESTIONS "
 				+ "order by questionId desc";
-		
-		RowMapper<Question> rm = new RowMapper<Question>() {
-			@Override
-			public Question mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Question(rs.getLong("questionId"),
+
+		return jdbcTemplate.query(
+				sql, (ResultSet rs, int rowNum) -> new Question(rs.getLong("questionId"),
 						rs.getString("writer"), rs.getString("title"), null,
 						rs.getTimestamp("createdDate"),
-						rs.getInt("countOfAnswer"));
-			}
-		};
-		
-		return jdbcTemplate.query(sql, rm);
+						rs.getInt("countOfAnswer"))
+		);
 	}
 
 	public Question findById(long questionId) {
 		String sql = "SELECT questionId, writer, title, contents, createdDate, countOfAnswer FROM QUESTIONS "
 				+ "WHERE questionId = ?";
 		
-		RowMapper<Question> rm = new RowMapper<Question>() {
-			@Override
-			public Question mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Question(rs.getLong("questionId"),
+		return jdbcTemplate.queryForObject(
+				sql, (ResultSet rs, int rowNum) ->
+				new Question(rs.getLong("questionId"),
 						rs.getString("writer"), rs.getString("title"),
 						rs.getString("contents"),
 						rs.getTimestamp("createdDate"),
-						rs.getInt("countOfAnswer"));
-			}
-		};
-		
-		return jdbcTemplate.queryForObject(sql, rm, questionId);
+						rs.getInt("countOfAnswer"))
+				, questionId);
 	}
 
 	public void update(Question question) {
